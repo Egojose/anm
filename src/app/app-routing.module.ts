d767829add9fa3971d6c0aa222bbdefb60f2cb47
Jose vasquez <josemicrobiologo@gmail.com>
2022-08-07 18:26:42 -0500

    crea ruta para saldos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FinProcesoComponent } from './components/fin-proceso/fin-proceso.component';
 import { FormularioDosComponent } from './components/formulario-dos/formulario-dos.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
+import { TablaSaldosComponent } from './components/tabla-saldos/tabla-saldos.component';
 
 
 const routes: Routes = [
   {path: 'compra', component: FormularioComponent},
   {path:'validacion', component: FormularioDosComponent},
-  {path: 'finalizacion', component: FinProcesoComponent}
+  {path: 'finalizacion', component: FinProcesoComponent},
+  {path: 'saldos', component: TablaSaldosComponent}
 ];
 
 @NgModule({
